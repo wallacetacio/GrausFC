f7b0adc5372cf6290c08eeec036c4ef7aaed0b30
Wallace Tácio <wallacett.dev@gmail.com>
2021-09-15 21:12:15 -0300

    Adiciona resolução do exercício Graus °F para °C ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fahrenheit;
+
+            Console.WriteLine(" ");
+            Console.Write("Olá Habitantes do planeta ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Terra!");
+            Console.ResetColor();
+            Console.WriteLine(" ");
+            Console.WriteLine(@"No decorrer desse programa, irei converter
+valores de Fahrenheit para Celsius.");
+            Console.WriteLine(" ");
+
+            Console.Write("Digite um valor..: ");
+            fahrenheit = Console.ReadLine();
+            double F = Convert.ToDouble(fahrenheit);
+            Console.WriteLine(" ");
+            Console.WriteLine($"°F = {F}");
+
+            double Dc = (F - 32) / 1.8;
+
+            Console.WriteLine(" ");
+            Console.WriteLine($"{fahrenheit}°F equivalem a {Dc}°C");
+        }
+    }
+}
